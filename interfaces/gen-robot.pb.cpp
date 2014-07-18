d531cca7510a2ff59e05abf14c1c810677706c39
David Ko <david.dk.ko@gmail.com>
2014-07-18 10:31:16 -0700

    Added interfaces, updated DOF_firmware, ribbon-bridge build files

---
+++ interfaces/gen-robot.pb.cpp
@@ -0,0 +1,12 @@
+#include "gen-robot.pb.hpp"
+#include "rpc/def.hpp"
+
+RPCDEF_CPP((barobo, Robot),
+        (ledColor)
+        ,
+        (ledColor)
+        ,
+        (ledColor)
+        ,
+        ,
+        )
